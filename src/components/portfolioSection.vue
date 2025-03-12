a81db341bc4ab38caddbb7404d2dc2f88f4aa894
ron19907 <ronaldmweema@gmail.com>
2025-03-12 16:10:45 +0200

    added a link to the portfolio projects and it's style in main.css

--- src/components/portfolioSection.vue
+++ src/components/portfolioSection.vue
@@ -16,10 +16,12 @@
             <div class="portfolio-content">
               <h3 class="portfolio-title">fitness app</h3>
               <p class="portfolio-category">Created with React</p>
-              
             </div>
-            <img src="../assets/fitness.jpg" alt="fitness image" class="portfolio-image-placeholder" />
-            <a href="https://nameless-trackingsystem.web.val.run">view</a>
+            <img src="../assets/workout-plan.jpg" alt="fitness image" class="portfolio-image-placeholder" />
+            <div class="link-container">
+              <a href="https://nameless-trackingsystem.web.val.run" class="project-link">view</a>
+              <arrow-right-icon class="icon" />
+            </div>
           </div>
             
           <div class="portfolio-item">
@@ -27,18 +29,25 @@
               <h3 class="portfolio-title"></h3>
               <p class="portfolio-category">Global cuisines</p>
             </div>
-            <div class="portfolio-image-placeholder"></div>
-            <a href="https://nameless-globalcuisinesite.web.val.run">view</a>
+            <img src="../assets/global-cuisines.png" class="portfolio-image-placeholder" />
+            <div class="link-container">
+              <a href="https://nameless-globalcuisinesite.web.val.run" class="project-link">view</a>
+              <arrow-right-icon class="icon" />
+            </div>
           </div>
           <div class="portfolio-item">
             <div class="portfolio-content">
-              <h3 class="portfolio-title">Energetic Tumbler.</h3>
-              <p class="portfolio-category">Product Development</p>
+              <h3 class="portfolio-title">Simple-Weather-App</h3>
+              <p class="portfolio-category">Product Developer</p>
             </div>
-            <div class="portfolio-image-placeholder"></div>
+            <img src="../assets/Simple-Weather-App.png" class="portfolio-image-placeholder"></div>
+            <div class="link-container">
+              <a href="https://https://nameless-weatherapp.web.val.run" class="project-link">view</a>
+              <arrow-right-icon class="icon" />
+            </div>
           </div>
         </div>
-      </div>
+      
     </section>
   </template>
   
